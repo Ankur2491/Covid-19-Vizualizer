fc73de190077771b206a4d843a3a801cdaa84e7f
ankur2491 <ankur.101045@gmail.com>
2020-06-19 11:46:17 +0530

    added last updated on

--- src/app/detail-country/detail-country.component.ts
+++ src/app/detail-country/detail-country.component.ts
@@ -14,11 +14,16 @@ export class DetailCountryComponent implements OnInit {
   @Input() modal_heading;
   @Input() modal_details;
   country_data: CountryDetails;
+  last_updated: string;
   constructor(private activeModal: NgbActiveModal, private http: HttpClient) { }
 
   ngOnInit(){
     this.http.get(`http://35.224.154.91:5630/countryData/${this.modal_heading}`).subscribe((data: CountryDetails)=>{
       this.country_data = data;
+      let lu = this.country_data.updated;
+      let date = new Date(0);
+      date.setUTCMilliseconds(lu);
+      this.last_updated = date.toDateString()+","+date.toLocaleTimeString();
     })
   }
   close() {
